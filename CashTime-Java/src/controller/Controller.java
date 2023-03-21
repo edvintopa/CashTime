a6dfbff3c2a8a01a15e1c7dc20ad4e5d90485584
AlanPapic <alan.papic@hotmail.com>
2023-03-21 15:09:14 +0100

    test2

--- CashTime-Java/src/controller/Controller.java
+++ CashTime-Java/src/controller/Controller.java
@@ -4,4 +4,8 @@ public class Controller {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
+
+    public void test2() {
+        System.out.println("Test2");
+    }
 }
