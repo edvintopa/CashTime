1b3dfaa5e06b9d0e558e865dabb53a736e8c1599
edvintopa <55089202+edvintopa@users.noreply.github.com>
2023-03-28 20:01:03 +0200

    Clean main

--- CashTime-Java/src/controller/Controller.java
+++ CashTime-Java/src/controller/Controller.java
@@ -1,17 +1,6 @@
 package controller;
 
 public class Controller {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
 
 
-    public void test1() {
-        System.out.println("Test1");
-    }
-
-    public void test2() {
-        System.out.println("Test2");
-
-    }
 }
