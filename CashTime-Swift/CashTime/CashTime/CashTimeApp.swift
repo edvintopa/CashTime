8699bebc8c2e34b21b9894cf964fc05c708cb7b0
edvintopa <55089202+edvintopa@users.noreply.github.com>
2023-03-28 22:01:53 +0200

    Swift init

---
+++ CashTime-Swift/CashTime/CashTime/CashTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashTimeApp.swift
+//  CashTime
+//
+//  Created by Edvin Topalovic on 2023-03-28.
+//
+
+import SwiftUI
+
+@main
+struct CashTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CashTime-Swift/CashTime/CashTime/TestFile.swift
@@ -0,0 +1,37 @@
+//
+//  TestFile.swift
+//  CashTime
+//
+//  Created by Edvin Topalovic on 2023-03-28.
+//
+
+import Foundation
+//
+//  ContentView.swift
+//  CashTime
+//
+//  Created by Edvin Topalovic on 2023-03-28.
+//
+
+import SwiftUI
+
+struct TestFile: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+        }
+        .padding()
+    }
+}
+
+struct TestFile_Previews: PreviewProvider {
+    static var previews: some View {
+        TestFile()
+    }
+}
