5951d36c787158b43ee0eaf94dff8dafa6cf5903
edvintopa <55089202+edvintopa@users.noreply.github.com>
2023-03-29 11:43:11 +0200

    Create Interval.java
    Updated with separate date and time objects.

---
+++ CashTime-Java/src/model/Interval.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Interval {
+    LocalDate date;
+    LocalTime start;
+    LocalTime end;
+
+    public Interval(LocalDate date, LocalTime start, LocalTime end) {
+        this.date = date;
+        this.start = start;
+        this.end = end;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getStart() {
+        return start;
+    }
+
+    public LocalTime getEnd() {
+        return end;
+    }
+
+    public void setStart(LocalTime start) {
+        this.start = start;
+    }
+
+    public void setEnd(LocalTime end) {
+        this.end = end;
+    }
+
+    public Duration getDuration() {
+        return Duration.between(start, end);
+    }
+}
