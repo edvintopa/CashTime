a6a5e54b1db0092a2fddb4d4f3e360d5259894c8
edvintopa <55089202+edvintopa@users.noreply.github.com>
2023-03-29 11:41:43 +0200

    Update Workplace.java

--- CashTime-Java/src/model/Workplace.java
+++ CashTime-Java/src/model/Workplace.java
@@ -1,8 +1,28 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Workplace {
     private String name;        //Name of workplace
-    private Double hourlyPay;   //Base hourly pay "grundlön"
+    private double hourlyPay;   //Base hourly pay "grundlön"
+    private List<OBInterval> rateSchedule;
 
+    public Workplace(String name, double pay) {
+        this.name = name;
+        this.hourlyPay = pay;
+        this.rateSchedule = new ArrayList<>();
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public double getPay() {
+        return hourlyPay;
+    }
+
+    public List<OBInterval> getRateSchedule() {
+        return rateSchedule;
+    }
 }
